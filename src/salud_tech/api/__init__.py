8adc65b309da7b99fc3c6ece46d1ea4e6c07aa3e
Cristhian <ccdelgadop@gmail.com>
2025-02-24 20:17:56 -0500

    add api

--- src/salud_tech/api/__init__.py
+++ src/salud_tech/api/__init__.py
@@ -1,22 +1,18 @@
 import os
 
-from flask import Flask, render_template, request, url_for, redirect, jsonify, session
+from flask import Flask, jsonify
 from flask_swagger import swagger
 
 # Identifica el directorio base
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 def registrar_handlers():
-    import aeroalpes.modulos.cliente.aplicacion
-    import aeroalpes.modulos.vuelos.aplicacion
+    # Importa los handlers
+    import salud_tech.modulos.procesamiento.aplicacion
 
 def importar_modelos_alchemy():
-    import aeroalpes.modulos.cliente.infraestructura.dto
-    import aeroalpes.modulos.hoteles.infraestructura.dto
-    import aeroalpes.modulos.pagos.infraestructura.dto
-    import aeroalpes.modulos.precios_dinamicos.infraestructura.dto
-    import aeroalpes.modulos.vehiculos.infraestructura.dto
-    import aeroalpes.modulos.vuelos.infraestructura.dto
+    # Import SQLAlchemy models
+    import salud_tech.modulos.procesamiento.infraestructura.dto
 
 def comenzar_consumidor():
     """
@@ -26,28 +22,13 @@ def comenzar_consumidor():
     """
 
     import threading
-    import aeroalpes.modulos.cliente.infraestructura.consumidores as cliente
-    import aeroalpes.modulos.hoteles.infraestructura.consumidores as hoteles
-    import aeroalpes.modulos.pagos.infraestructura.consumidores as pagos
-    import aeroalpes.modulos.precios_dinamicos.infraestructura.consumidores as precios_dinamicos
-    import aeroalpes.modulos.vehiculos.infraestructura.consumidores as vehiculos
-    import aeroalpes.modulos.vuelos.infraestructura.consumidores as vuelos
+    import salud_tech.modulos.procesamiento.infraestructura.consumidores as procesamiento
 
     # Suscripción a eventos
-    threading.Thread(target=cliente.suscribirse_a_eventos).start()
-    threading.Thread(target=hoteles.suscribirse_a_eventos).start()
-    threading.Thread(target=pagos.suscribirse_a_eventos).start()
-    threading.Thread(target=precios_dinamicos.suscribirse_a_eventos).start()
-    threading.Thread(target=vehiculos.suscribirse_a_eventos).start()
-    threading.Thread(target=vuelos.suscribirse_a_eventos).start()
+    threading.Thread(target=procesamiento.suscribirse_a_eventos).start()
 
     # Suscripción a comandos
-    threading.Thread(target=cliente.suscribirse_a_comandos).start()
-    threading.Thread(target=hoteles.suscribirse_a_comandos).start()
-    threading.Thread(target=pagos.suscribirse_a_comandos).start()
-    threading.Thread(target=precios_dinamicos.suscribirse_a_comandos).start()
-    threading.Thread(target=vehiculos.suscribirse_a_comandos).start()
-    threading.Thread(target=vuelos.suscribirse_a_comandos).start()
+    threading.Thread(target=procesamiento.suscribirse_a_comandos).start()
 
 def create_app(configuracion={}):
     # Init la aplicacion de Flask
@@ -62,10 +43,10 @@ def create_app(configuracion={}):
     app.config['TESTING'] = configuracion.get('TESTING')
 
      # Inicializa la DB
-    from aeroalpes.config.db import init_db
+    from salud_tech.config.db import init_db
     init_db(app)
 
-    from aeroalpes.config.db import db
+    from salud_tech.config.db import db
 
     importar_modelos_alchemy()
     registrar_handlers()
@@ -76,20 +57,10 @@ def create_app(configuracion={}):
             comenzar_consumidor()
 
      # Importa Blueprints
-    from . import cliente
-    from . import hoteles
-    from . import pagos
-    from . import precios_dinamicos
-    from . import vehiculos
-    from . import vuelos
+    from . import procesamiento
 
     # Registro de Blueprints
-    app.register_blueprint(cliente.bp)
-    app.register_blueprint(hoteles.bp)
-    app.register_blueprint(pagos.bp)
-    app.register_blueprint(precios_dinamicos.bp)
-    app.register_blueprint(vehiculos.bp)
-    app.register_blueprint(vuelos.bp)
+    app.register_blueprint(procesamiento.bp)
 
     @app.route("/spec")
     def spec():

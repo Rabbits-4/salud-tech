3dee17c2aeadab0615a45fbbb992eec7ee4f3547
Cristhian <ccdelgadop@gmail.com>
2025-02-24 23:12:11 -0500

    update

--- src/salud_tech/api/procesamiento.py
+++ src/salud_tech/api/procesamiento.py
@@ -1,7 +1,22 @@
+from flask import Response, request
 import aeroalpes.seedwork.presentacion.api as api
 
 bp = api.crear_blueprint('procesamiento', '/procesamiento')
 
-@bp.route('/reserva-comando', methods=('POST',))
-def hanble_imagen():
-    ...
+@bp.route('/crear-dataset-medico-comando', methods=('POST'))
+def crear_dataset_medico():
+    try:
+        dataset_dict = request.json
+
+        map_dataset = MapeadorDatasetDTOJson()
+        dataset_dto = map_dataset.externo_a_dto(dataset_dict)
+
+        comando = CrearDatasetMedico(dataset_dto.fecha_creacion, dataset_dto.fecha_actualizacion, dataset_dto.id, dataset_dto.pacientes, dataset_dto.medicos, dataset_dto.consultas)
+        
+        # TODO Reemplaze es todo código sincrono y use el broker de eventos para propagar este comando de forma asíncrona
+        # Revise la clase Despachador de la capa de infraestructura
+        ejecutar_commando(comando)
+        
+        return Response('{}', status=202, mimetype='application/json')
+    except ExcepcionDominio as e:
+        return Response(json.dumps(dict(error=str(e))), status=400, mimetype='application/json')

99f939026c2e2d85ff85a2e8dc9c321183270c10
Cristhian <ccdelgadop@gmail.com>
2025-03-07 22:05:03 -0500

    fix

--- src/mapear/modulos/mapear/aplicacion/__init__.py
+++ src/mapear/modulos/mapear/aplicacion/__init__.py
@@ -2,6 +2,6 @@ from pydispatch import dispatcher
 
 from .handlers import HandlerParquetIntegracion
 
-from mapear.modulos.mapear.dominio.eventos import DatasetCreado
+from mapear.modulos.mapear.dominio.eventos import ParquetCreado
 
-dispatcher.connect(HandlerParquetIntegracion.handle_parquet_creado, signal=f'{DatasetCreado.__name__}Integracion')
+dispatcher.connect(HandlerParquetIntegracion.handle_parquet_creado, signal=f'{ParquetCreado.__name__}Integracion')

4075b2c6b77a378eb54aefd4f7866234355b3168
Cristhian <ccdelgadop@gmail.com>
2025-03-08 16:03:19 -0500

    add listener

--- src/mapear/modulos/mapear/infraestructura/consumidores.py
+++ src/mapear/modulos/mapear/infraestructura/consumidores.py
@@ -8,6 +8,7 @@ import traceback
 from mapear.modulos.mapear.infraestructura.schema.v1.eventos import EventoDicomAnonimoCreado
 from mapear.modulos.mapear.infraestructura.schema.v1.comandos import ComandoCrearParquet
 from mapear.seedwork.infraestructura import utils
+from mapear.modulos.mapear.aplicacion.comandos.create_parquet import CreateParquet
 
 def suscribirse_a_eventos():
     cliente = None
@@ -19,7 +20,20 @@ def suscribirse_a_eventos():
         while True:
             mensaje = consumidor.receive()            
             print(f'Evento recibido: {mensaje.value().data}')
-            logging.error("**** dicom-anonimizado", mensaje.value().data)
+            logging.error("**** dicom-anonimizado")
+            data = mensaje.value().data
+            logging.error("**** DATA", data)
+
+            CreateParquet(
+                entorno_clinico=data.entorno_clinico,
+                registro_de_diagnostico=data.registro_de_diagnostico,
+                fecha_creacion=data.fecha_creacion,
+                fecha_actualizacion=data.fecha_actualizacion,
+                historial_paciente_id=str(uuid.uuid4()),
+                contexto_procesal=data.contexto_procesal,
+                notas_clinicas=data.notas_clinicas,
+                data=data.data
+            )
 
             consumidor.acknowledge(mensaje)     
 

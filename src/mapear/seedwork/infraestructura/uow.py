e6de6243fa2d066c4c8c8536780fb92bd343c8a1
Cristhian <ccdelgadop@gmail.com>
2025-03-08 19:12:34 -0500

    change

--- src/mapear/seedwork/infraestructura/uow.py
+++ src/mapear/seedwork/infraestructura/uow.py
@@ -5,6 +5,7 @@ from mapear.seedwork.dominio.entidades import AgregacionRaiz
 from pydispatch import dispatcher
 
 import pickle
+import logging
 
 
 class Lock(Enum):
@@ -64,10 +65,12 @@ class UnidadTrabajo(ABC):
         self._publicar_eventos_dominio(batch)
 
     def _publicar_eventos_dominio(self, batch):
+        logging.error(f'🔥 Publicando eventos dominio')
         for evento in self._obtener_eventos(batches=[batch]):
             dispatcher.send(signal=f'{type(evento).__name__}Dominio', evento=evento)
 
     def _publicar_eventos_post_commit(self):
+        logging.error(f'🔥 Publicando eventos integracion')
         for evento in self._obtener_eventos():
             dispatcher.send(signal=f'{type(evento).__name__}Integracion', evento=evento)
 
